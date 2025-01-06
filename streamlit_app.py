7b46519242fcdce0bf814935bca9d3ef9fe46963
jldfernando <jldfernando@protonmail.com>
2025-01-06 13:16:00 +0800

    reorganized some files

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,29 @@ import streamlit as st
 
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
+    
+valid_accounts = {
+    'Therese': 'ringring',
+    'Gabby': 'ringring',
+    'Josh': 'ringring',
+    'JMC': 'TBA'
+}
+
+account_classes = {
+    'Capts': ['Therese','Gabby','Josh'],
+    'Guest': ['JMC']
+}
 
 ## Login functions
 def login():
+    UN = st.text_input('Username')
+    PW = st.text_input('Password', type="password")
     if st.button("Log in"):
-        st.session_state.logged_in = True
-        st.rerun()
+        if UN in valid_accounts.keys() and PW == valid_accounts[UN]:
+            st.session_state.logged_in = True
+            st.rerun()
+        else:
+            st.write('Invalid username or password')
 
 def logout():
     if st.button("Log out"):
@@ -18,7 +35,7 @@ def logout():
 login_page = st.Page(login, title="Log in", icon=":material/login:")
 logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 overview = st.Page(
-    "Schedule Tracker/overview.py", title="Schedule Overview", icon=":material/Schedule:", default=True
+    "Schedule Tracker/Schedule Overview.py", title="Schedule Overview", icon=":material/schedule:", default=True
 )
 
 
